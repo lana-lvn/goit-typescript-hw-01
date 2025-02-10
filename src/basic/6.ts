e6e9da6a2cb7bd057de6bfeae9372b56884a88d0
ruslana <ruslana.lohvyn@gmail.com>
2025-02-10 18:15:36 +0200

    init

---
+++ src/basic/6.ts
@@ -0,0 +1,15 @@
+const mango = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
